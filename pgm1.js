f96d5986c123b44eae802a5061c5db4da5dbcb6d
abiyababy8 <abiyababy16@gmail.com>
2025-01-31 10:17:45 +0530

    added some code

---
+++ pgm1.js
@@ -0,0 +1,12 @@
+// console.log("hello")
+// let arr=[1,2,3];
+// for(let i=0;i<=arr.length;i++){
+//     console.log(i);
+// }
+// console.log("finished")
+
+
+console.log("API call stated");
+//call api and get user details from backend
+callUserDetails();
+console.log("finished")
